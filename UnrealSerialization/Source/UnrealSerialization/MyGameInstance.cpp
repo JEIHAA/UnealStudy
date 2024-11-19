c68a6ec5e765997a91fdb6435389442f32bc3fe0
HuiEon Lee <61311298+JEIHAA@users.noreply.github.com>
2024-11-19 17:24:42 +0900

    UnrealPackage

--- UnrealSerialization/Source/UnrealSerialization/MyGameInstance.cpp
+++ UnrealSerialization/Source/UnrealSerialization/MyGameInstance.cpp
@@ -5,6 +5,16 @@
 #include "Student.h"
 #include "JsonObjectConverter.h"
 // 언리얼 오브젝트를 Json 오브젝트로 편리하게 변환해주는 헬퍼 라이브러리들이 들어있음
+#include "UObject/SavePackage.h"
+// 패키지 저장 옵션 지정
+
+// 패키지 이름을 지정할 때 알아야하는 규칙
+// 언리얼 엔진 프로젝트를 기동하면 각각 고유한 경로를 가지게 됨
+// 그 고유한 경로 중 하나가 /Game
+// 게임에서 사용되는 에셋들을 모아놓은 대표 폴더
+// /Saved는 /Temp에 매핑되어 있음
+const FString UMyGameInstance::PackageName = TEXT("/Game/Student"); // /Game/패키지 이름
+const FString UMyGameInstance::AssetName = TEXT("TopStudent"); // Student 패키지가 관리할 메인 애셋이름
 
 // 출력함수
 void PrintStudentInfo(const UStudent* InStudent, const FString& InTag)
@@ -14,6 +24,22 @@ void PrintStudentInfo(const UStudent* InStudent, const FString& InTag)
 
 UMyGameInstance::UMyGameInstance()
 {
+    // 생성자에서 애셋 로딩
+    // 생성자에서 애셋을 로딩한다는 것은
+    // 이 애셋은 게임이 시작하기 전에 미리 메모리에 다 올라와 있어야한다는 것
+    const FString TopSoftObjectPath = FString::Printf(TEXT("%s.%s"), *PackageName, *AssetName);
+    
+    // 생성자에서 애셋을 로딩할 때는 LoadObject()를 사용하지 않고
+    // 언리얼에서 제공하는 ConstructorHelpers라는 것을 사용
+    // ConstructorHelpers의 FObjectFinder를 사용해 애셋 지정
+    static ConstructorHelpers::FObjectFinder<UStudent> UASSET_TopStudent(*TopSoftObjectPath);
+    // ConstructorHelpers에 의해 해당 애셋을 찾아서 로딩이 됐다면
+    // Succeeded() 함수를 통해 로딩이 제대로 됐는지 확인할 수 있음
+    if (UASSET_TopStudent.Succeeded())
+    {
+        // .Object라는 멤버변수를 사용해 로딩된 애셋에 접근할 수 있음
+        PrintStudentInfo(UASSET_TopStudent.Object, TEXT("Constructor"));
+    }
 }
 
 void UMyGameInstance::Init()
@@ -191,5 +217,122 @@ void UMyGameInstance::Init()
         }
     }
 
+    SaveStudentPackage();
+    //LoadStudentPackage();
+    //LoadStudentObject();
+
+    // 비동기 방식 애셋 로딩
+    const FString TopSoftObjectPath = FString::Printf(TEXT("%s.%s"), *PackageName, *AssetName);
+    Handle = StreamableManager.RequestAsyncLoad(TopSoftObjectPath,
+        // 핸들 참조 레퍼런스 가져옴
+        [&]()
+        {
+            // 핸들이 유효한지, 핸들이 로딩이 끝났는지 검사
+            if (Handle.IsValid() && Handle->HasLoadCompleted())
+            {
+                // 핸들을 통해 객체 가져오기
+                // 언리얼 오브젝트를 받아 오는데 형변환을 해서 Student인지 검사
+                UStudent* TopStudent = Cast<UStudent>(Handle->GetLoadedAsset());
+                // Student가 맞다면 출력
+                if (TopStudent)
+                {
+                    PrintStudentInfo(TopStudent, TEXT("AsyncLoad"));
+
+                    // 다 쓴 핸들은 닫아주기
+                    Handle->ReleaseHandle();
+                    Handle.Reset();
+                }
+            }
+        }
+    );
+}
+
+void UMyGameInstance::SaveStudentPackage() const
+{
+    // 패키지가 이미 있다면 저장하기 전에 다 로딩해준 뒤에 저장하는 것이 더 안전함
+    UPackage* StudentPackage = ::LoadPackage(nullptr, *PackageName, LOAD_None);
+    if (StudentPackage)
+    {
+        StudentPackage->FullyLoad();
+    }
+
+    // 패키지 생성
+    StudentPackage = CreatePackage(*PackageName);
+
+    // 패키지 저장
+    // 패키지를 저장하는 enum
+    // 일반적으로  RF_Public, RF_Standalone 이라는 값을 사용함
+    EObjectFlags ObjectFlag = RF_Public | RF_Standalone;
+
+    // 패키지에 어떤 내용을 담을지 지정
+    UStudent* TopStudent = NewObject<UStudent>(StudentPackage, UStudent::StaticClass(), *AssetName, ObjectFlag);
+    // NewObject<UStudent>()로 생성하면 인자가 아무것도 안들어가는 경우
+    // 트랜지언트 패키지라고 하는 임시 패키지 안에 언리얼 오브젝트가 저장됨
+    // StudentPackage 안에 넣어야하기 때문에 첫번째 인자로 패키지를 직접 지정할 수 있음
+    // 이름은 세번째 인자로 들어가기 때문에 두번째 인자를 클래스 정보를 넣어 강제로 채움
+    // 세번째 인자로 오브젝트의 고유한 이름을 지정
+    // 네번째 인자로 플래그 지정
+    TopStudent->SetName(TEXT("이득우"));
+    TopStudent->SetOrder(36);
+
+    // TopStudent의 서브 오브젝트 지정
+    const int32 NumOfSubs = 10;
+    for (int32 ix = 1; ix <= NumOfSubs; ++ix)
+    {
+        FString SubObjectName = FString::Printf(TEXT("Student%d"), ix);
+        // TopStudent 안에 들어가도록 서브 오브젝트 생성
+        UStudent* SubStudent = NewObject<UStudent>(TopStudent, UStudent::StaticClass(), *SubObjectName, ObjectFlag);
+        SubStudent->SetName(FString::Printf(TEXT("학생%d"), ix));
+        SubStudent->SetOrder(ix);
+    }
+
+    // LongPackageNameToFilename: 패키지에 관련된 파일 이름을 생성할 수 있는 함수
+    // GetAssetPackageExtension: 언리얼 엔진에서 지정한 uasset이라는 확장자를 의미
+    const FString PackageFileName = FPackageName::LongPackageNameToFilename(PackageName, FPackageName::GetAssetPackageExtension());
+    // 저장 옵션 지정
+    FSavePackageArgs SaveArgs;
+    SaveArgs.TopLevelFlags = ObjectFlag;
+
+    // 패키지 저장
+    // SavePackage(생성한 패키지, nullptr, 파일이름)
+    // 두번째 인자는 앞에서 설정했음 (애셋관련)
+    // 저장에 관련된 파라미터와 아규먼트만 넣어주면 됨
+    if (UPackage::SavePackage(StudentPackage, nullptr, *PackageFileName, SaveArgs))
+    {
+        UE_LOG(LogTemp, Log, TEXT("패키지가 성공적으로 저장되었습니다."));
+    }
+     
+}
+
+void UMyGameInstance::LoadStudentPackage() const
+{
+    //LoadPackage(패키지 정보 없음, 패키지 이름, 로드 설정 기본)
+    UPackage* StudentPackage = ::LoadPackage(nullptr, *PackageName, LOAD_None);
+    if (nullptr == StudentPackage)
+    {
+        UE_LOG(LogTemp, Log, TEXT("패키지를 찾을 수 없습니다."));
+        return;
+    }
+
+    // 패키지를 찾으면 안에 가지고 있는 애셋을 모두 다 로딩하는 함수를 실행
+    StudentPackage->FullyLoad();
+
+    // 패키지가 로딩되면 로딩된 패키지 안에서 원하는 오브젝트, 애셋을 찾을 수 있음
+    // FindObject<UStudent>(찾을 패키지 지정, 애셋 이름)
+    // 패키지 안에서 UStudent 타입의 언리얼 오브젝트를 찾아서 반환함
+    UStudent* TopStudent = FindObject<UStudent>(StudentPackage, *AssetName);
+    PrintStudentInfo(TopStudent, TEXT("FindObject Asset"));
+}
+
+
+// 패키지를 로드하지 않고 경로를 사용해 애셋 로딩
+void UMyGameInstance::LoadStudentObject() const
+{
+    // 애셋의 오브젝트 경로 {패키지 이름}.{애셋이름}
+    const FString TopSoftObjectPath = FString::Printf(TEXT("%s.%s"),*PackageName, *AssetName);
+
+    // 패키지를 로딩하지 않기 때문에 첫번째 인자 null, 두번째에 바로 경로 정보
+    UStudent* TopStudent = LoadObject<UStudent>(nullptr, *TopSoftObjectPath);
+    PrintStudentInfo(TopStudent, TEXT("LoadObject Asset"));
 }
 
--- UnrealSerialization/Source/UnrealSerialization/MyGameInstance.h
+++ UnrealSerialization/Source/UnrealSerialization/MyGameInstance.h
@@ -4,8 +4,9 @@
 
 #include "CoreMinimal.h"
 #include "Engine/GameInstance.h"
+#include "Engine/StreamableManager.h"
+// 비동기 로딩
 #include "MyGameInstance.generated.h"
-
 
 struct FStudentData
 {
@@ -36,8 +37,27 @@ public:
 
 	virtual void Init() override;
 
+	// 패키지 저장함수
+	void SaveStudentPackage() const;
+	// 패키지 로드함수
+	void LoadStudentPackage() const;
+
+	// 패키지를 로드하지 않고 경로를 사용해 애셋 로딩 함수
+	void LoadStudentObject() const;
+
 private:
 	UPROPERTY()
 	// 데이터를 저장할 소스 오브젝트
 	TObjectPtr<class UStudent> StudentSrc;
+
+	// 패키지를 사용하기 위해서는
+	// 패키지와 패키지가 담고 있는 대표 에셋을 설정해주어야함
+	// 패키지와 패키지가 담고 있는 대표 에셋의 이름 지정
+	static const FString PackageName;
+	static const FString AssetName;
+
+	// FStreamableManager는 포인터가 아니고 그냥 선언해주는 것이라 헤더 추가해야함
+	FStreamableManager StreamableManager;
+	// 스트리밍된 애셋을 관리할 핸들
+	TSharedPtr<FStreamableHandle> Handle;
 };
