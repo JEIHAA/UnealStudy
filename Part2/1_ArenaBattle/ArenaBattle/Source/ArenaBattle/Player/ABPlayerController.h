eaf433583c349f522c3b71896942df3fffd836a6
HuiEon Lee <61311298+JEIHAA@users.noreply.github.com>
2024-11-22 18:20:10 +0900

    UnrealBase

---
+++ Part2/1_ArenaBattle/ArenaBattle/Source/ArenaBattle/Player/ABPlayerController.h
@@ -0,0 +1,21 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/PlayerController.h"
+#include "ABPlayerController.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class ARENABATTLE_API AABPlayerController : public APlayerController
+{
+	GENERATED_BODY()
+	
+protected:
+	// 게임 시작 시 실행되는 함수
+	virtual void BeginPlay() override;
+	
+};
